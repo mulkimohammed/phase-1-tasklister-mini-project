164121f24affbb40f1d8cfe17af8ceb8cc574d14
Mulki <aweismulki@gmail.com>
2026-01-07 17:37:19 +0300

    frst commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+   const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const li = document.createElement("li");
+    li.textContent = input.value;
+
+    taskList.appendChild(li);
+    form.reset();
+  });
 });
